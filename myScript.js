cbb119d3a397677a943faf5ccb93e90f599ddb09
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-07-13 09:51:23 -0600

    Minor changes.

--- myScript.js
+++ myScript.js
@@ -1,7 +1,8 @@
 function loadAbout() {
     event.preventDefault();
+    let aboutText = "Andrew Kepson is a web developer from Colorado Springs, Colorado."
     let aboutTitle = $('<h2>').text('About Me');
-    let aboutBody = $('<p>').text("about me text");
+    let aboutBody = $('<p>').text(aboutText);
     $('.container').html(aboutTitle,aboutBody);
 }
 
@@ -14,8 +15,9 @@ function loadPortfolio() {
 function loadContact() {
     event.preventDefault();
     let form = $('<form>');
-    let inputName = $('<input type="text" id="name-input" placeholder="name">');
-    let inputEmail = $('<input type="text" id="email-input" placeholder="e-mail address">');
-    let inputMessage = $('<input type="textarea" id="message-input" placeholder="Please do not hesitate to reach out with any inquiries!">');
-    $('.container').html(form).append(inputName).append(inputEmail).append(inputMessage);
+    let inputName = $('<input type="text" id="name-input" cols="50" placeholder="name">');
+    let inputEmail = $('<p>' + '<input type="email" id="email-input" cols="50" placeholder="e-mail address">');
+    let inputMessage = $('<p>' + '<textarea id="message-input" rows="5" cols="50" placeholder="Please do not hesitate to reach out with any inquiries!">');
+    let submitButton = $('<p>' + '<button type="submit" id="submit-button">');
+    $('.container').html(form).append(inputName).append(inputEmail).append(inputMessage).append(submitButton);
 }
