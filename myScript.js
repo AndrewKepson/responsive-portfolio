ee636ca98313c0adaea3c64865a77150f782d318
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-07-13 21:40:48 -0600

    Removed unnecessary pages. Added hidden sections and functions to call them to page.

--- myScript.js
+++ myScript.js
@@ -1,23 +1,21 @@
 function loadAbout() {
     event.preventDefault();
-    let aboutText = "Andrew Kepson is a web developer from Colorado Springs, Colorado."
-    let aboutTitle = $('<h2>').text('About Me');
-    let aboutBody = $('<p>').text(aboutText);
-    $('.container').html(aboutTitle,aboutBody);
+    $('#contact-form').hide();
+    $('#portfolio-grid').hide();
+    $('#about-me').show();
+    
 }
 
 function loadPortfolio() {
     event.preventDefault();
-    let portfolio = $('<div class="" id="portfolio-content">');
-    $('.container').html(portfolio);
+    $('#contact-form').hide();
+    $('#about-me').hide();
+    $('#portfolio-grid').show();
 }
 
 function loadContact() {
     event.preventDefault();
-    let form = $('<form>');
-    let inputName = $('<input type="text" id="name-input" cols="50" placeholder="name">');
-    let inputEmail = $('<p>' + '<input type="email" id="email-input" cols="50" placeholder="e-mail address">');
-    let inputMessage = $('<p>' + '<textarea id="message-input" rows="5" cols="50" placeholder="Please do not hesitate to reach out with any inquiries!">');
-    let submitButton = $('<p>' + '<button type="submit" id="submit-button">');
-    $('.container').html(form).append(inputName).append(inputEmail).append(inputMessage).append(submitButton);
+    $('#about-me').hide();
+    $('#portfolio-grid').hide();
+    $('#contact-form').show();
 }
